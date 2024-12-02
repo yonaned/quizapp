f92bf67ec117117082049696df2765df977818cc
yonaned <yossangirma@gmail.com>
2024-12-02 14:03:04 +0300

    navigator.js

--- js/navigator.js
+++ js/navigator.js
@@ -5,21 +5,21 @@
 
 // Adding click event listeners to the icons
 document.getElementById('home_icon').onclick = function() {
-    navigate('home.html');
+    navigate('index.html');
 };
 document.getElementById('about_icon').onclick = function() {
-    navigate('Pages/about.html');
+    navigate('pages/about.html');
 };
 document.getElementById('history_icon').onclick = function() {
-    navigate('Pages/history.html');
+    navigate('pages/history.html');
 };
 document.getElementById('psychology_icon').onclick = function() {
-    navigate('Pages/psychology.html');
+    navigate('pages/psychology.html');
 };
 
 document.getElementById('history_iconh').onclick = function() {
-    navigate('Pages/history.html');
+    navigate('pages/history.html');
 };
 document.getElementById('psychology_iconh').onclick = function() {
-    navigate('Pages/psychology.html');
+    navigate('pages/psychology.html');
 };
